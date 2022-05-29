c72c56c1e56eeeea9541a129cf849e0f5e8e731a
kevin <kevinmibz6@gmail.com>
2022-05-29 23:33:29 +0300

    first commmit

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -134,7 +134,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR, 'staticfiles'
-django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
