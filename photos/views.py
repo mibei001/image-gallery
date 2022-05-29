a8b4b1fbd3f8d256c61bdc1afc6c8d2ea7852402
kevin <kevinmibz6@gmail.com>
2022-05-29 19:56:12 +0300

    creating add photos view

--- photos/views.py
+++ photos/views.py
@@ -21,6 +21,28 @@ def loginUser(request):
     return render(request, 'photos/login_register.html', {'page': page})
 
 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+
+            if user is not None:
+                login(request, user)
+                return redirect('gallery')
+
+    context = {'form': form, 'page': page}
+    return render(request, 'photos/login_register.html', context)
+
 
 def gallery(request):
     user = request.user
@@ -40,7 +62,39 @@ def viewPhoto(request, pk):
     photo = Photo.objects.get(id=pk)
     return render(request, 'photos/photo.html', {'photo': photo})
 
- def search_venues(request):
+
+def addPhoto(request):
+    user = request.user
+
+    categories = user.category_set.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        images = request.FILES.getlist('images')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(
+                user=user,
+                name=data['category_new'])
+        else:
+            category = None
+
+        for image in images:
+            photo = Photo.objects.create(
+                category=category,
+                description=data['description'],
+                image=image,
+            )
+
+        return redirect('gallery')
+
+    context = {'categories': categories}
+    return render(request, 'photos/add.html', context)
+
+
+def search_venues(request):
     if request.method == "POST":
         searched = request.POST['searched']
         photos = Photo.objects.filter(
@@ -57,16 +111,8 @@ def viewPhoto(request, pk):
                       {})
 
 
-  # Delete a Venue
+# Delete a Venue
 def delete_venue(request, photo_id):
     venue = Photo.objects.get(pk=photo_id)
     venue.delete()
-    return redirect('gallery')                    
-   
-
-
-
-
-
-
-
+    return redirect('gallery')
