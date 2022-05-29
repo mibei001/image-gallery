a5b4442417b9a2a2084299380f9d4768158c4fff
kevin <kevinmibz6@gmail.com>
2022-05-29 19:28:57 +0300

    creating the photo model

--- photos/models.py
+++ photos/models.py
@@ -17,4 +17,15 @@ class Category(models.Model):
         return self.name
 
 
+class Photo(models.Model):
+    class Meta:
+        verbose_name = 'Photo'
+        verbose_name_plural = 'Photos'
 
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
