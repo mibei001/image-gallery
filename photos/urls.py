8c8cf7ad0d24f6eca75b99194ee6f6d4c34f6eeb
kevin <kevinmibz6@gmail.com>
2022-05-29 19:50:25 +0300

    creating view photo details view

--- photos/urls.py
+++ photos/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  
+    path('login/', views.loginUser, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),
+
     path('', views.gallery, name='gallery'),
+    path('photo/<str:pk>/', views.viewPhoto, name='photo'),
+    path('add/', views.addPhoto, name='add'),
+    path('search_venues', views.search_venues, name='search-venues'),
+    path('delete_venue/<photo_id>', views.delete_venue, name='delete-venue'),
+
 
 
 ]
